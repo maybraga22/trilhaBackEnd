b53cbe3bfbcabda6abc0e56a634d0624e4a9ec5b
Mayara Braga <maybraga22@gmail.com>
2021-12-27 18:01:20 -0300

    atualizazção 27/12

---
+++ desafio.dois.poo/src/application/Program.java
@@ -0,0 +1,84 @@
+package application;
+
+import entities.Category;
+import entities.Entry;
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		Category category1 = new Category(1, "Salário", "Reecebimento de salário");
+		Category category2 = new Category();
+		category2.setId(2);
+		category2.setName("Pagamento");
+		category2.setDescription("Pagamento de Boletos");
+		
+
+		Entry entry1 = new Entry(1, "Salário na Empresa X", "Adiantamento quinzenal", "revenue", "4405.49", "15/12/2021",
+				true, 1);
+
+		Entry entry2 = new Entry(0, null, null, null, null, null, false, 0);
+		entry2.setId(2);
+		entry2.setName("Salário da Empresa Y");
+		entry2.setDescription("Pagamento mensal");
+		entry2.setType("revenue");
+		entry2.setAmount("4405.49");
+		entry2.setDate("30/12/2021");
+		entry2.setPaid(true);
+		entry2.setCategoryId(2);
+
+		System.out.println(category1.toString());
+		System.out.println(category2.toString());
+		System.out.println();
+		System.out.println(entry1.toString());
+		System.out.println(entry2.toString());
+		System.out.println();
+
+		List<Product> product1 = new ArrayList<>();
+
+		Product p1 = new Product(1, "BATEDEIRA", "PHILIPS", 300.00);
+		Product p2 = new Product(2, "LIQUIDIFICADOR", "BRITANIA", 230.00);
+		Product p3 = new Product(3, "MICROONDAS", "LG", 400.00);
+		Product p4 = new Product(4, "AIR FRYER", "PHILIPS", 350.00);
+		Product p5 = new Product(5, "CAFETEIRA", "NESPRESSO", 120.00);
+
+		product1.add(p1);
+		product1.add(p2);
+		product1.add(p3);
+		product1.add(p4);
+		product1.add(p5);
+
+		for (Product p : product1) {
+			p.productPrice();
+			System.out.println(p);
+		}
+
+		List<Product> product2 = new ArrayList<>();
+		
+		Product pr1 = new Product(1, "TV", "SAMSUNG", 3000.00);
+		Product pr2 = new Product(2, "HOME THEATHER", "JBL", 2300.00);
+		Product pr3 = new Product(3, "COMPUTADOR", "DELL", 4000.00);
+
+		product2.add(pr1);
+		product2.add(pr2);
+		product2.add(pr3);		
+		
+		System.out.println();
+		System.out.println("Produtor mais caros:");
+
+		for (Product p : product2) {
+			p.productPrice();
+			System.out.println(p);
+		}
+
+
+
+	}
+
+}
---
+++ desafio.dois.poo/src/entities/Entry.java
@@ -0,0 +1,101 @@
+package entities;
+
+public class Entry {
+	private long id;
+	private String name;
+	private String description;
+	private String type;
+	private String amount;
+	private String date;
+	private boolean paid;
+	private long categoryId;
+
+
+	public Entry(long id, String name, String description, String type, String amount, String date, boolean paid,
+			long categoryId) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.type = type;
+		this.amount = amount;
+		this.date = date;
+		this.paid = paid;
+		this.categoryId = categoryId;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String dedsceiption) {
+		this.description = dedsceiption;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public boolean isPaid() {
+		return paid;
+	}
+
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	@Override
+	public String toString() {
+		return "Entry [id=" + id + 
+				", name=" + name + 
+				", dedsceiption=" + description + 
+				", type=" + type + 
+				", amount=" + amount + 
+				", date=" + date + 
+				", paid=" + paid + 
+				", categoryId=" + categoryId + "]";
+	}
+}
