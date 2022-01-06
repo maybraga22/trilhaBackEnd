2cc2b4fca082d1946e53ee7e188f023086e6794b
Mayara Braga <maybraga22@gmail.com>
2022-01-06 12:58:42 -0300

    desafio 6

---
+++ desafioSeis/src/main/java/com/trilha/back/financys/services/CategoryService.java
@@ -0,0 +1,43 @@
+package com.trilha.back.financys.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trilha.back.financys.entities.Category;
+import com.trilha.back.financys.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public List<Category> findAll() {
+		return categoryRepository.findAll();
+	}
+
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+
+	public Long idCategoryByName(String categoryName) {
+		Category read = categoryRepository.findByName(categoryName);
+		return read.getId();
+	}
+
+	public Category save(Category category) {
+		return categoryRepository.save(category);
+	}
+
+	public Optional<Category> findById(Long id) {
+		return categoryRepository.findById(id);
+	}
+
+	public void delete(Long id) {
+		categoryRepository.deleteById(id);
+	}
+
+}
